31231441ee5bcaa50303c6073fd263bbbd809f6d
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2022-09-25 16:30:53 +0700

    add top bar

--- CalendarApp/CalendarApp/ViewModels/Schedule/ScheduleViewModel.cs
+++ CalendarApp/CalendarApp/ViewModels/Schedule/ScheduleViewModel.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using CalendarApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace CalendarApp.ViewModels.Schedule
 {
     public class ScheduleViewModel : BaseViewModel
     {
+        private ObservableCollection<DayTitle> _days;
+        public ObservableCollection<DayTitle> days
+        {
+            get { return _days; }
+            set { _days = value; OnPropertyChanged(); }
+        }
+
+        public ScheduleViewModel()
+        {
+            days = new ObservableCollection<DayTitle>();
+            days.Add(new DayTitle { title = "Hai", day = 26 });
+            days.Add(new DayTitle { title = "Ba", day = 27 });
+            days.Add(new DayTitle { title = "Tư", day = 28 });
+            days.Add(new DayTitle { title = "Năm", day = 29 });
+            days.Add(new DayTitle { title = "Sáu", day = 30 });
+            days.Add(new DayTitle { title = "Bảy", day = 1 });
+            days.Add(new DayTitle { title = "CN", day = 2 });
+        }
+    }
+    public class DayTitle
+    {
+        public string title { get; set; }
+        public int day { get; set; }
     }
 }
