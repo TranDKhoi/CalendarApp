2cbe7fe8f98a7f4086e24314996261d4b184d0fe
TranDKhoi <90909489+TranDKhoi@users.noreply.github.com>
2022-10-04 14:45:27 +0700

    Squashed commit of the following:
    commit 79f3f442abf4fb17d1bcee3902f62b9054796057
    Author: TranDKhoi <90909489+TranDKhoi@users.noreply.github.com>
    Date:   Sun Sep 25 16:32:33 2022 +0700
    
        Squashed commit of the following:
    
        commit 2c7e82dbaaa87d91495e9aa77d4f2168ddb4c666
        Author: Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
        Date:   Sun Sep 25 14:58:17 2022 +0700
    
            add bottom bar
    
        commit 99e4ca15ab394efca2ddd08b6148d2e2c7146c4a
        Author: Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
        Date:   Sun Sep 25 13:15:58 2022 +0700
    
            add material icon
    
    commit 055a21709f2ea28ed3fb891a0666548d88eb597f
    Author: TranDKhoi <90909489+TranDKhoi@users.noreply.github.com>
    Date:   Sun Sep 25 13:32:33 2022 +0700
    
        Squashed commit of the following:
    
        commit 99e4ca15ab394efca2ddd08b6148d2e2c7146c4a
        Author: Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
        Date:   Sun Sep 25 13:15:58 2022 +0700
    
            add material icon
    
        minor fix icon and ui
    
    commit 0de7b250bdafac91c88a3ef5b8a5cb068401c663
    Author: TranDKhoi <90909489+TranDKhoi@users.noreply.github.com>
    Date:   Sun Sep 25 12:34:51 2022 +0700
    
        done UI

--- CalendarApp/CalendarApp/ViewModels/Authen/LoginViewModel.cs
+++ CalendarApp/CalendarApp/ViewModels/Authen/LoginViewModel.cs
@@ -1,10 +1,58 @@
-﻿using System;
+﻿using Acr.UserDialogs;
+using CalendarApp.Views.Authen;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace CalendarApp.ViewModels.Authen
 {
-    public class LoginViewModel:BaseViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; OnPropertyChanged(); }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; OnPropertyChanged(); }
+        }
+
+        public Command LoginCM { get; set; }
+        public Command ToForgotScreenCM { get; set; }
+        public Command ToSignupScreenCM { get; set; }
+        public LoginViewModel()
+        {
+            LoginCM = new Command(() =>
+            {
+                if (isValidData())
+                {
+                    //API here
+                }
+                else
+                {
+                    UserDialogs.Instance.Toast("Vui lòng điền đầy đủ thông tin");
+                }
+            });
+            ToForgotScreenCM = new Command(() =>
+            {
+                Application.Current.MainPage.Navigation.PushAsync(new ForgotPassScreen());
+            });
+            ToSignupScreenCM = new Command(() =>
+            {
+                Application.Current.MainPage.Navigation.PushAsync(new SignupScreen());
+
+            });
+        }
+
+        private bool isValidData()
+        {
+            return !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password);
+        }
     }
 }
---
+++ CalendarApp/CalendarApp/ViewModels/Authen/SignupViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Acr.UserDialogs;
+using CalendarApp.Views.Authen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarApp.ViewModels.Authen
+{
+    public class SignupViewModel : BaseViewModel
+    {
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; OnPropertyChanged(); }
+        }
+
+        private string pass;
+        public string Pass
+        {
+            get { return pass; }
+            set { pass = value; OnPropertyChanged(); }
+        }
+
+        private string rePass;
+        public string RePass
+        {
+            get { return rePass; }
+            set { rePass = value; OnPropertyChanged(); }
+        }
+
+        private int verifyCode;
+        public int VerifyCode
+        {
+            get { return verifyCode; }
+            set { verifyCode = value; OnPropertyChanged(); }
+        }
+
+
+        public Command ToVerifyScreenCM { get; set; }
+        public Command PopCM { get; set; }
+        public Command VerifyCodeCM { get; set; }
+
+        public SignupViewModel()
+        {
+            ToVerifyScreenCM = new Command(() =>
+            {
+                if (!isValidData())
+                {
+                    UserDialogs.Instance.Toast("Vui lòng điền đủ thông tin");
+                    return;
+                }
+                if (RePass != Pass)
+                {
+                    UserDialogs.Instance.Toast("Mật khẩu không khớp");
+                    return;
+                }
+                if (RePass.Length < 6)
+                {
+                    UserDialogs.Instance.Toast("Mật khẩu tối thiểu 6 ký tự");
+                    return;
+                }
+
+                Application.Current.MainPage.Navigation.PushAsync(new VerifySignup(this));
+
+            });
+            PopCM = new Command(() =>
+            {
+                Application.Current.MainPage.Navigation.PopAsync();
+            });
+            VerifyCodeCM = new Command(() =>
+            {
+                //
+            });
+        }
+
+        private bool isValidData()
+        {
+            return !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Pass) && !string.IsNullOrEmpty(RePass);
+        }
+    }
+}
