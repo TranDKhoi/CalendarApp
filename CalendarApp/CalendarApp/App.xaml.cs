76cd99d1eab1a3e74e900c5de3be524728e96378
TranDKhoi <90909489+TranDKhoi@users.noreply.github.com>
2022-10-11 09:26:33 +0700

    .
    .

--- CalendarApp/CalendarApp/App.xaml.cs
+++ CalendarApp/CalendarApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace CalendarApp
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginScreen());
+            MainPage = new NavigationPage(new BottomBarCustom());
         }
 
         protected override void OnStart()
